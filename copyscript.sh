ab842210879b9d4ee9e8f346628a879fc8d51fdf
Kristian <soapsudsxx@protonmail.com>
2023-01-07 00:27:52 -0700

    "Added Ctaps to most classes with Right Click. Modified some of the Engie binds. Changed all configs to bind modifyers with .cfg files"

---
+++ copyscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/jump.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/offline_jump.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/speedrun.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_nm2.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_jump.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_engie.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_nm2.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_jump.cfg" ./
+cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_engie.cfg" ./
+cp /mnt/Storage/SteamLibrary/steamapps/common/Team\ Fortress\ 2/tf/cfg/overrides/* ./overrides/
