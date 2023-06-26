0841ffd249021b09806f8636b1f1df3cfc4ed40d
Kristian <soapsudsxx@protonmail.com>
2023-06-25 19:09:58 -0600

    updates

--- copyscript.sh
+++ copyscript.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/jump.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/offline_jump.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/speedrun.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_nm2.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_jump.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_engie.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_nm2.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_jump.cfg" ./
-cp "/mnt/Storage/SteamLibrary/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_engie.cfg" ./
-cp /mnt/Storage/SteamLibrary/steamapps/common/Team\ Fortress\ 2/tf/cfg/overrides/* ./overrides/
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/jump.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/offline_jump.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/speedrun.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasinit.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_nm2.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_jump.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasinit_engie.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_nm2.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_jump.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_engie.cfg" ./
+cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/overrides/*" ./overrides/
