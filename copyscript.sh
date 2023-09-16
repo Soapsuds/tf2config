ee2be758847ea345fd88563ba9b7de1a129972bf
Kristian <soapsudsxx@protonmail.com>
2023-09-16 11:22:10 -0600

    Demo was broken. I had some demoknight specific changes but forgot to back them up. Switch back to nm2 for now. Also fixed the copy script and changed some settings in modules

--- copyscript.sh
+++ copyscript.sh
@@ -11,4 +11,4 @@ cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps
 cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_nm2.cfg" ./
 cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_jump.cfg" ./
 cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/aliasoverride_engie.cfg" ./
-cp "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/overrides/*" ./overrides/
+cp -r "/home/kristian/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Team Fortress 2/tf/cfg/overrides/" ./
